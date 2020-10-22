9cf50a3e1c05e9c831aee526694408028c47c06a
Célestin Ballèvre <celestin.ballevre@gmail.com>
2020-10-22 15:38:46 +0200

    Commit before adding vue-tailwind

---
+++ components/ui/form/Datetime.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="flex">
+	  	<input type="date">
+	  	<input type="time" class="ml-4 w-20">
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Datetime'
+}
+</script>
--- pages/events/add.vue
+++ pages/events/add.vue
@@ -5,22 +5,16 @@
 	  	<div class="container mt-6 px-4">
 	  		<form>
 	  			<form-group label="Titre de l'évènement">
-	  				<form-input type="text" placeholder="Ajouter un titre" />
+	  				<form-input type="text" placeholder="Ajouter un titre" v-model="title"/>
 	  			</form-group>
 	  			<form-group>
-	  				<form-checkbox>Toute la journée ?</form-checkbox>
+	  				<form-input-checkbox v-model="is_all_day">Toute la journée ?</form-input-checkbox>
 	  			</form-group>
 	  			<form-legend label="Débute">
-	  				<div class="flex">
-	  					<input type="date">
-	  					<input type="time" class="ml-4 w-20">
-	  				</div>
+	  				<form-input-datetime v-model="starts_at" />
 	  			</form-legend>
 	  			<form-legend label="Prend fin">
-	  				<div class="flex">
-	  					<input type="date">
-	  					<input type="time" class="ml-4 w-20">
-	  				</div>
+	  				<form-input-datetime v-model="ends_at" />
 	  			</form-legend>
 	  			<form-group label="Organisateur">
 	  				<form-input type="text" />
@@ -29,9 +23,7 @@
 	  				<form-input type="text" />
 	  			</form-group>
 	  			<form-group label="Notes (facultatif)">
-	  				<textarea class="block w-full ">
-	  					
-	  				</textarea>
+	  				<textarea class="block w-full " />
 	  			</form-group>
 	  			<button class="fixed bottom-0 left-0 right-0 mb-20 w-full bg-indigo-600 py-3 text-white">Enregistrer</button>
 	  		</form>
@@ -47,6 +39,10 @@ export default {
   data () {
     return {
     	'title' : null,
+    	'is_all_day': false,
+    	'starts_at': new Date(),
+    	'ends_at': new Date(),
+    	'notes' : null
     }
   }
 }

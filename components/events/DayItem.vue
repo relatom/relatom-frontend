1a799bacdc94ae95dfc711faea52e7f7f5b073e7
Célestin Ballèvre <celestin.ballevre@gmail.com>
2020-10-26 15:51:10 +0100

    Updated design index

--- components/events/DayItem.vue
+++ components/events/DayItem.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="bg-white px-4 py-3 border-l border-r border-b cursor-pointer" v-bind:class="{ 'border-t rounded-t-2xl': first, 'rounded-b-2xl': last }" v-on:click="onClick">	
-		<h3><nuxt-link :to="'/events/'+item.id">{{ item.title }}</nuxt-link></h3>
+		<h3 class="font-bold text-lg"><nuxt-link :to="'/events/'+item.id">{{ item.title }}</nuxt-link></h3>
 		<p class="text-gray-600 text-sm">{{ item.starts_at | luxon:format('HH:mm', { serverFormat: 'sql' }) }} - {{ item.ends_at | luxon:format('HH:mm', { serverFormat: 'sql' })}}</p>
 	</div>
 </template>

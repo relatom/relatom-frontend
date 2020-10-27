b17c1cbc92f56c59b5585c84c0b5a3db16583f20
Célestin Ballèvre <celestin.ballevre@gmail.com>
2020-10-27 07:16:34 +0100

    WIP data retrieve by backend

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-  	<sub-header title="Évènements (10)">
+  	<sub-header :title="'Évènements (' + events.metadata.total + ')'">
   		<template v-slot:actions>
   			<btn-link to="/events/add">
   				<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
@@ -9,7 +9,8 @@
   		</template>
   	</sub-header>	
   	<container>
-      <events-day v-for="(items, day) in events" :day="day" :items="items" :key="'day-' + day"></events-day>
+      <events-day v-for="(items, day) in events.data" :day="day" :items="items" :key="'day-' + day"></events-day>
+      <t-button>Show everything up to {{ events.metadata.next_date }}</t-button>
   	</container>
   </main>
 </template>

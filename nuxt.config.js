53086a65769678d747d38990b590be8dd1bd4309
Célestin Ballèvre <celestin.ballevre@gmail.com>
2020-10-29 11:34:07 +0100

    WIP add validation with vee-validation

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '~/plugins/vue-tailwind',
-    '~/plugins/vue-luxon'
+    '~/plugins/vue-luxon',
+    "~/plugins/vee-validate.js"
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -36,6 +37,10 @@ export default {
     },
     {
       path: '~/components/ui/'
+    },
+    {
+      path: '~/components/ui/t',
+      prefix: 'T'
     },
     {
       path: '~/components/ui/form',
@@ -61,6 +66,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    transpile: ["vee-validate/dist/rules"],
   },
 
   i18n: {
--- pages/events/add.vue
+++ pages/events/add.vue
@@ -2,52 +2,56 @@
 	<main>
 	  	<sub-header :title="$t('events.title.add')" />	
 	  	<container>
-	  		<form
-	  			@submit="checkForm"
-	  			method="post">
-	  			<t-input-group
-	  				:label="$t('events.label.title')" >
-	  				<t-input 
-	  				type="text" 
-	  				v-model="title" 
-	  				:placeholder="$t('events.placeholder.title')" />
-	  			</t-input-group>
-	  			<t-input-group>
-	  				<t-checkbox 
-	  				wrapped 
-	  				:label="$t('events.label.is_all_day')" 
-	  				v-model="is_all_day" />
-	  			</t-input-group>
-	  			<t-input-group 
-	  				:label="$t('events.label.starts_at')">
-		  			<t-input 
-		  				type="date" 
-		  				v-model="starts_at_date" />
-		  			<t-input 
-		  				type="time" 
-		  				v-model="starts_at_time" 
-		  				v-if="!is_all_day" />
-	  			</t-input-group>
-	  			<t-input-group 
-	  				:label="$t('events.label.ends_at')">
-		  			<t-input 
-		  				type="date" 
-		  				v-model="ends_at_date" />
-		  			<t-input 
-		  				type="time" 
-		  				v-model="ends_at_time" 
-		  				v-if="!is_all_day" />
-	  			</t-input-group>
-	  			<t-input-group 
-	  				:label="$t('events.label.notes')" >
-	  				<t-textarea 
-	  					v-model="notes" 
-	  					:placeholder="$t('events.placeholder.notes')" />
-	  			</t-input-group>
-	  			<t-button 
-	  				type="submit" 
-	  				variant="full">{{ $t('events.submit.add') }}</t-button>
-	  		</form>
+	  		<ValidationObserver v-slot="{ invalid }">
+		  		<form
+		  			@submit="checkForm"
+		  			method="post">
+		  			<t-input-with-validation 
+		  				rules="required"
+		  				type="string"
+		  				:label="$t('events.label.title')"
+		  				:placeholder="$t('events.placeholder.title')"
+		  				name="title"
+		  				v-model="title"
+		  				description="We'll never share your email with anyone else"
+		  			/>
+		  			<t-input-group>
+		  				<t-checkbox 
+		  				wrapped 
+		  				:label="$t('events.label.is_all_day')" 
+		  				v-model="is_all_day" />
+		  			</t-input-group>
+		  			<t-input-group 
+		  				:label="$t('events.label.starts_at')">
+			  			<t-input 
+			  				type="date" 
+			  				v-model="starts_at_date" />
+			  			<t-input 
+			  				type="time" 
+			  				v-model="starts_at_time" 
+			  				v-if="!is_all_day" />
+		  			</t-input-group>
+		  			<t-input-group 
+		  				:label="$t('events.label.ends_at')">
+			  			<t-input 
+			  				type="date" 
+			  				v-model="ends_at_date" />
+			  			<t-input 
+			  				type="time" 
+			  				v-model="ends_at_time" 
+			  				v-if="!is_all_day" />
+		  			</t-input-group>
+		  			<t-input-group 
+		  				:label="$t('events.label.notes')" >
+		  				<t-textarea 
+		  					v-model="notes" 
+		  					:placeholder="$t('events.placeholder.notes')" />
+		  			</t-input-group>
+		  			<t-button 
+		  				type="submit" 
+		  				variant="full">{{ $t('events.submit.add') }}</t-button>
+		  		</form>
+	  		</ValidationObserver>
 	  	</container>
   	</main>
 </template>
@@ -55,9 +59,11 @@
 <script>
 
 import { DateTime } from 'luxon';
+import { ValidationObserver } from "vee-validate";
 
 export default {
   	name: 'add',
+  	components : { ValidationObserver },
 	data () {
 		const currentDateTime =  DateTime.local();
 		return {

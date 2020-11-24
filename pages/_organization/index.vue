1f3a60b1d91c22172c8738b6ab736c0bca24881b
cballevre <celestin.ballevre@gmail.com>
2020-11-24 08:46:18 +0100

    organization WIP

---
+++ pages/_organization/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="mt-6 mx-auto w-10">
+      <h1 class="text-xl">
+        Nom du groupe
+      </h1>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	auth: 'guest'
+}
+</script>
